5379d0049b6a570eca7cb330216bf48bdb44789b
pietraldo <pietrzakjakub4321@gmail.com>
2025-03-15 21:52:52 +0100

    updated create table students

--- PoZiomkaInfrastructure/Migrations/CreateTables.sql
+++ PoZiomkaInfrastructure/Migrations/CreateTables.sql
@@ -1,6 +1,14 @@
 ﻿CREATE TABLE Students (
 	Id int PRIMARY KEY IDENTITY(1, 1) NOT NULL,
 	Email varchar(100) UNIQUE NOT NULL,
+	FirstName varchar(100) NOT NULL,
+	LastName varchar(100) NOT NULL,
 	PasswordHash binary(60) NOT NULL,
-	IsConfirmed bit NOT NULL
+	IsConfirmed bit NOT NULL,
+	PhoneNumber varchar(20) NULL,
+	IndexNumber varchar(20) NULL,
+	ReservationId int NULL,
+	HasAcceptedReservation bit NULL,
+	IsPhoneNumberHidden bit NOT NULL,
+	IsIndexNumberHidden bit NOT NULL,
 );
