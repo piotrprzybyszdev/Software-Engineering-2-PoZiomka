b89803ca58dddddc537e74ff4a6b832f0495d510
Piotr Przybysz <piotr.przybysz.dev@gmail.com>
2025-03-15 19:41:03 +0100

    Configured frontend for deployment

--- PoZiomkaApp/src/app/app.config.ts
+++ PoZiomkaApp/src/app/app.config.ts
@@ -1,6 +1,7 @@
-import { provideHttpClient } from "@angular/common/http";
+import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
 import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from "@angular/core";
 import { provideRouter, withComponentInputBinding } from "@angular/router";
+import { routingInterceptorProvider } from "./routing-interceptor";
 import { routes } from "./app.routes";
 import { registerLocaleData } from "@angular/common";
 import localePL from "@angular/common/locales/pl";
@@ -11,7 +12,8 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withComponentInputBinding()),
-    provideHttpClient(),
+    provideHttpClient(withInterceptorsFromDi()),
+    routingInterceptorProvider,
     { provide: LOCALE_ID, useValue: 'pl-PL' }
   ]
 }
