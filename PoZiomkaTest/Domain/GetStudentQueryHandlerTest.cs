88ced011a7469dd0fa98ca839757c798e130ffbf
pietraldo <pietrzakjakub4321@gmail.com>
2025-03-31 21:59:34 +0200

    added test titles

--- PoZiomkaTest/Domain/GetStudentQueryHandlerTest.cs
+++ PoZiomkaTest/Domain/GetStudentQueryHandlerTest.cs
@@ -9,5 +9,45 @@ namespace PoZiomkaTest.Domain;
 
 public class GetStudentQueryHandlerTest
 {
-	
+	[Fact]
+	public async Task AdminGetAccess()
+	{
+		Assert.True(false);
+	}
+	[Fact]
+	public async Task UserItSelfGetAccess()
+	{
+		Assert.True(false);
+	}
+	[Fact]
+	public async Task UserWithMatchGetAccess()
+	{
+		Assert.True(false);
+	}
+	[Fact]
+	public async Task UserWithoutMatchDontGetAccess()
+	{
+		Assert.True(false);
+	}
+	[Fact]
+	public async Task AdminGetsHiddenInfo()
+	{
+		Assert.True(false);
+	}
+	[Fact]
+	public async Task UserGetsItSelfHiddenInfo()
+	{
+		Assert.True(false);
+	}
+	[Fact]
+	public async Task UserWithMatchDontGetsHiddentInfo()
+	{
+		Assert.True(false);
+	}
+
+	[Fact]
+	public async Task ThrowExceptionIfUserIdNotExists()
+	{
+		Assert.True(false);
+	}
 }
--- PoZiomkaTest/Domain/UpdateStudentCommandHandlerTest.cs
+++ PoZiomkaTest/Domain/UpdateStudentCommandHandlerTest.cs
@@ -9,5 +9,24 @@ namespace PoZiomkaTest.Domain;
 
 public class UpdateStudentCommandHandlerTest
 {
-   
+	[Fact]
+	public async Task AdminGetAccess()
+	{
+		Assert.True(false);
+	}
+	[Fact]
+	public async Task UserItSelfGetAccess()
+	{
+		Assert.True(false);
+	}
+	[Fact]
+	public async Task UserCanNotEditAnotherUser()
+	{
+		Assert.True(false);
+	}
+	[Fact]
+	public async Task ThrowExceptionIfUserIdNotExists()
+	{
+		Assert.True(false);
+	}
 }
