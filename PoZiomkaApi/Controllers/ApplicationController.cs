bb2301cd9c2a29da5cd1c1c60a48a622e8709e44
Piotr Przybysz <piotr.przybysz.dev@gmail.com>
2025-04-01 00:58:36 +0200

    setup controllers and requests

---
+++ PoZiomkaApi/Controllers/ApplicationController.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PoZiomkaApi.Common;
+using PoZiomkaApi.Requests.Application;
+using PoZiomkaDomain.Application.Dtos;
+using PoZiomkaDomain.Common;
+
+namespace PoZiomkaApi.Controllers;
+
+[Route("/application")]
+[ApiController]
+public class ApplicationController(IMediator mediator) : ControllerBase
+{
+    [HttpPost("submit/{id}")]
+    [Authorize(Roles = Roles.Student)]
+    public async Task<IActionResult> Submit(int id, IFormFile file)
+    {
+        return NotFound();
+    }
+
+    [HttpPut("resolve/{id}")]
+    [Authorize(Roles = Roles.Administrator)]
+    public async Task<IActionResult> Resolve([FromBody] ResolveRequest resolveRequest)
+    {
+        return NotFound();
+    }
+
+    [HttpGet("get")]
+    public async Task<IActionResult> Get()
+    {
+        return Ok(new List<ApplicationDisplay>([new ApplicationDisplay(
+            1, 3, new ApplicationTypeModel(1, "Test application type", "Test application type description"), new NetworkFile(), ApplicationStatus.Pending
+        )]));
+    }
+}
---
+++ PoZiomkaApi/Controllers/RoomController.cs
@@ -0,0 +1,56 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PoZiomkaApi.Requests.Room;
+using PoZiomkaDomain.Common;
+using PoZiomkaDomain.Room.Dtos;
+using PoZiomkaDomain.Student.Dtos;
+
+namespace PoZiomkaApi.Controllers;
+
+[Route("/room")]
+[ApiController]
+public class RoomController(IMediator mediator) : ControllerBase
+{
+    [HttpGet("get")]
+    [Authorize(Roles = Roles.Administrator)]
+    public async Task<IActionResult> Get()
+    {
+        return Ok(new List<RoomDisplay>([new RoomDisplay(
+            1, 3, 311, 5, [
+                new StudentDisplay(1, "test@gmail.com", "Jan", "Kowalski", "333444", "777888999", null, null, 1, false, false)
+            ]
+        )]));
+    }
+
+    [HttpGet("get/{id}")]
+    [Authorize(Roles = $"{Roles.Student},{Roles.Administrator}")]
+    public async Task<IActionResult> GetById(int id)
+    {
+        return Ok(new RoomDisplay(
+            1, 3, 311, 5, [
+                new StudentDisplay(1, "test@gmail.com", "Jan", "Kowalski", "333444", "777888999", null, null, 1, false, false)
+            ]
+        ));
+    }
+
+    [HttpPost("create")]
+    [Authorize(Roles = Roles.Administrator)]
+    public async Task<IActionResult> Create([FromBody] CreateRequest createRequest)
+    {
+        return NotFound();
+    }
+
+    [HttpPut("update")]
+    [Authorize(Roles = Roles.Administrator)]
+    public async Task<IActionResult> Update([FromBody] UpdateRequest updateRequest)
+    {
+        return NotFound();
+    }
+
+    [HttpDelete("delete/{id}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        return NotFound();
+    }
+}
