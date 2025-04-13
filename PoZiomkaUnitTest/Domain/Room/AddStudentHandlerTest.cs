787298716dfac799a261e514b5c2458863b4805a
pietraldo <pietrzakjakub4321@gmail.com>
2025-04-13 23:28:36 +0200

    add student handler test

--- PoZiomkaUnitTest/Domain/Room/AddStudentHandlerTest.cs
+++ PoZiomkaUnitTest/Domain/Room/AddStudentHandlerTest.cs
@@ -3,6 +3,7 @@ using PoZiomkaDomain.Common.Exceptions;
 using PoZiomkaDomain.Room;
 using PoZiomkaDomain.Room.Commands.AddStudent;
 using PoZiomkaDomain.Room.Commands.AddStudentToRoom;
+using PoZiomkaDomain.Room.Dtos;
 using PoZiomkaDomain.Room.Exceptions;
 using PoZiomkaDomain.Student;
 using PoZiomkaDomain.Student.Dtos;
@@ -97,7 +98,45 @@ public class AddStudentHandlerTest
     [Fact]
     public async Task RoomOverflowThrowsException()
     {
-        Assert.True(false);
+        var studentModel = new StudentModel(
+           Id: 1,
+           Email: "student@example.com",
+           FirstName: "John",
+           LastName: "Doe",
+           PasswordHash: "hashed_password_value",
+           IsConfirmed: true,
+           PhoneNumber: "123-456-7890",
+           IndexNumber: "S123456",
+           ReservationId: 10,
+           HasAcceptedReservation: true,
+           RoomId: null,
+           IsPhoneNumberHidden: false,
+           IsIndexNumberHidden: false
+       );
+
+        var roomModel = new RoomModel(
+            Id: 101,
+            Floor: 2,
+            Number: 205,
+            Capacity: 3
+        );
+
+        var studentRepository = new Mock<IStudentRepository>();
+        studentRepository.Setup(x => x.GetStudentById(It.IsAny<int>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(studentModel);
+        studentRepository.Setup(x=> x.GetStudentsByRoomId(It.IsAny<int>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(new List<StudentModel> {studentModel, studentModel, studentModel}); 
+
+
+        var roomRepository = new Mock<IRoomRepository>();
+        roomRepository.Setup(x => x.GetRoomById(It.IsAny<int>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(roomModel);
+
+        AddStudentCommand studentCommand = new AddStudentCommand(1, 1);
+        AddStudentCommandHandler handler = new(roomRepository.Object, studentRepository.Object);
+
+        await Assert.ThrowsAsync<RoomFullException>(async () =>
+                   await handler.Handle(studentCommand, new CancellationToken()));
     }
 }
 
