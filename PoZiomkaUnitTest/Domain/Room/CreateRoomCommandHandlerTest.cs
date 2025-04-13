377d71f094ebee55df2bbe6991b33e1b6f8d25a8
pietraldo <pietrzakjakub4321@gmail.com>
2025-04-13 23:31:35 +0200

    room create test

--- PoZiomkaUnitTest/Domain/Room/CreateRoomCommandHandlerTest.cs
+++ PoZiomkaUnitTest/Domain/Room/CreateRoomCommandHandlerTest.cs
@@ -1,5 +1,24 @@
-﻿namespace PoZiomkaUnitTest.Domain.Room;
+﻿using Moq;
+using PoZiomkaDomain.Room;
+using PoZiomkaDomain.Room.Commands.CreateRoom;
+using PoZiomkaDomain.Room.Dtos;
+using PoZiomkaDomain.Room.Exceptions;
+
+namespace PoZiomkaUnitTest.Domain.Room;
 public class CreateRoomCommandHandlerTest
 {
+    [Fact]
+    public async Task RoomNumberNotUniqueThrowsException()
+    {
+        var roomRepository = new Mock<IRoomRepository>();
+        roomRepository.Setup(x => x.CreateRoom(It.IsAny<RoomCreate>(), It.IsAny<CancellationToken>()))
+            .ThrowsAsync(new RoomNumberNotUniqueException());
+
+        CreateRoomCommand roomCommand = new CreateRoomCommand(1, 1, 1);
+        CreateRoomCommandHandler handler = new(roomRepository.Object);
+
+        await Assert.ThrowsAsync<RoomNumberTakenException>(async () =>
+                          await handler.Handle(roomCommand, new CancellationToken()));
+    }
 }
 
