7f2cc4b49fd77c24af18db48dc4fc04d62a61884
pietraldo <pietrzakjakub4321@gmail.com>
2025-04-14 00:30:56 +0200

    get room test

--- PoZiomkaUnitTest/Domain/Room/RemoveStudentCommandHandlerTest.cs
+++ PoZiomkaUnitTest/Domain/Room/RemoveStudentCommandHandlerTest.cs
@@ -1,20 +1,104 @@
-﻿namespace PoZiomkaUnitTest.Domain.Room;
+﻿using Moq;
+using PoZiomkaDomain.Room.Commands.AddStudent;
+using PoZiomkaDomain.Room.Commands.AddStudentToRoom;
+using PoZiomkaDomain.Room.Dtos;
+using PoZiomkaDomain.Room.Exceptions;
+using PoZiomkaDomain.Room;
+using PoZiomkaDomain.Student.Dtos;
+using PoZiomkaDomain.Student;
+using PoZiomkaDomain.Form.Dtos;
+using PoZiomkaDomain.Common.Exceptions;
+using PoZiomkaDomain.Room.Commands.RemoveStudent;
+using PoZiomkaDomain.Student.Exceptions;
+using PoZiomkaInfrastructure.Repositories;
+
+namespace PoZiomkaUnitTest.Domain.Room;
 public class RemoveStudentCommandHandlerTest
 {
     [Fact]
     public async Task StudentNotFoundException()
     {
-        Assert.True(false);
+        var studentRepository = new Mock<IStudentRepository>();
+        studentRepository.Setup(x => x.GetStudentById(It.IsAny<int>(), It.IsAny<CancellationToken>()))
+            .ThrowsAsync(new IdNotFoundException());
+
+
+        var roomRepository = new Mock<IRoomRepository>();
+
+        RemoveStudentCommand studentCommand = new RemoveStudentCommand(1, 1);
+        RemoveStudentCommandHandler handler = new(roomRepository.Object, studentRepository.Object);
+
+        await Assert.ThrowsAsync<StudentNotFoundException>(async () =>
+                   await handler.Handle(studentCommand, new CancellationToken()));
     }
     [Fact]
     public async Task StudentNotAssignedToRoomThrowsException()
     {
-        Assert.True(false);
+        var studentModel = new StudentModel(
+          Id: 1,
+          Email: "student@example.com",
+          FirstName: "John",
+          LastName: "Doe",
+          PasswordHash: "hashed_password_value",
+          IsConfirmed: true,
+          PhoneNumber: "123-456-7890",
+          IndexNumber: "S123456",
+          ReservationId: 10,
+          HasAcceptedReservation: true,
+          RoomId: 101,
+          IsPhoneNumberHidden: false,
+          IsIndexNumberHidden: false
+      );
+
+        var roomModel = new RoomModel(
+            Id: 101,
+            Floor: 2,
+            Number: 205,
+            Capacity: 3
+        );
+
+        var studentRepository = new Mock<IStudentRepository>();
+        studentRepository.Setup(x => x.GetStudentById(It.IsAny<int>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(studentModel);
+
+        var roomRepository = new Mock<IRoomRepository>();
+
+        RemoveStudentCommand studentCommand = new RemoveStudentCommand(102, 1);
+        RemoveStudentCommandHandler handler = new(roomRepository.Object, studentRepository.Object);
+
+        await Assert.ThrowsAsync<StudentNotAssignedToRoomException>(async () =>
+                   await handler.Handle(studentCommand, new CancellationToken()));
     }
     [Fact]
     public async Task StudentAssignedToRoomIsOk()
     {
-        Assert.True(false);
+        var studentModel = new StudentModel(
+              Id: 1,
+              Email: "student@example.com",
+              FirstName: "John",
+              LastName: "Doe",
+              PasswordHash: "hashed_password_value",
+              IsConfirmed: true,
+              PhoneNumber: "123-456-7890",
+              IndexNumber: "S123456",
+              ReservationId: 10,
+              HasAcceptedReservation: true,
+              RoomId: 101,
+              IsPhoneNumberHidden: false,
+              IsIndexNumberHidden: false
+          );
+
+        var studentRepository = new Mock<IStudentRepository>();
+        studentRepository.Setup(x => x.GetStudentById(It.IsAny<int>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(studentModel);
+
+        var roomRepository = new Mock<IRoomRepository>();
+
+        RemoveStudentCommand studentCommand = new RemoveStudentCommand(101, 1);
+        RemoveStudentCommandHandler handler = new(roomRepository.Object, studentRepository.Object);
+
+        await handler.Handle(studentCommand, new CancellationToken());
+        Assert.True(true);
     }
 }
 
