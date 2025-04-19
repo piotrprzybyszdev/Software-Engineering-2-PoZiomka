6e69838aa57840d3528907693870587ea97fde6d
pietraldo <pietrzakjakub4321@gmail.com>
2025-04-19 14:57:12 +0200

    IFormRepository

---
+++ PoZiomkaDomain/Form/IFormRepository.cs
@@ -0,0 +1,15 @@
+﻿using PoZiomkaDomain.Form.Dtos;
+
+namespace PoZiomkaDomain.Form;
+
+public interface IFormRepository
+{
+    Task<IEnumerable<FormModel>> GetForms(CancellationToken? cancellationToken);
+    Task<FormDisplay> GetFormDisplay(int id, CancellationToken? cancellationToken);
+    Task CreateForm(string title, CancellationToken? cancellationToken);
+    Task CreateFormObligatoryPreferences(int formId, string name, CancellationToken? cancellationToken);
+    Task CreateFormObligatoryPreferenceOptions(int preferenceId, string name, CancellationToken? cancellationToken);
+    Task UpdateForm(int id, string title, CancellationToken? cancellationToken);
+    Task DeleteForm(int id, CancellationToken? cancellationToken);
+}
+
