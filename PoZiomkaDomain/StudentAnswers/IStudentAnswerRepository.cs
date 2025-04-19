11c8b9cb7b824ca6729515bd9fa00f0032d728b0
pietraldo <pietrzakjakub4321@gmail.com>
2025-04-19 15:16:44 +0200

    IStudentAnswerRepository

---
+++ PoZiomkaDomain/StudentAnswers/IStudentAnswerRepository.cs
@@ -0,0 +1,11 @@
+﻿using PoZiomkaDomain.StudentAnswers.Dtos;
+
+namespace PoZiomkaDomain.StudentAnswers;
+
+public interface IStudentAnswerRepository
+{
+    // Will be used during update of form, because if we update form
+    // and there will be no more preference with this id, we need to delete all amswers to this preference
+    Task DeleteAllAnswersToPreference(int preferenceId, CancellationToken? cancellationToken);
+}
+
