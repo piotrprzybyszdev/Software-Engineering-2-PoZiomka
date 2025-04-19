6e82b5c9af98944ab9b34beff27d5e7d302ac2af
pietraldo <pietrzakjakub4321@gmail.com>
2025-04-19 18:24:44 +0200

    IStudentRepository

---
+++ PoZiomkaDomain/StudentAnswers/IStudentAnswerRepository.cs
@@ -0,0 +1,13 @@
+﻿using PoZiomkaDomain.StudentAnswers.Dtos;
+
+namespace PoZiomkaDomain.StudentAnswers;
+
+public interface IStudentAnswerRepository
+{
+    Task<StudentAnswerStatus> GetStudentFormAnswerStatus(int studentId, CancellationToken? cancellationToken);
+    Task<StudentAnswerDisplay> GetStudentAnswer(int formId, int studentId, CancellationToken? cancellationToken);
+    Task CreateAnswer(StudentAnswerCreateData data, CancellationToken? cancellationToken);
+    Task UpdateAnswer(StudentAnswerUpdateData data, CancellationToken? cancellationToken);
+    Task DeleteAnswer(int answerId, CancellationToken? cancellationToken);
+}
+
