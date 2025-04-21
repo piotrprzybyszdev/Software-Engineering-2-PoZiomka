993e1b08432efb3f94e00bc8af94a03de4479448
pietraldo <pietrzakjakub4321@gmail.com>
2025-04-21 21:56:02 +0200

    get student answers

--- PoZiomkaInfrastructure/Repositories/StudentAnswerRepository.cs
+++ PoZiomkaInfrastructure/Repositories/StudentAnswerRepository.cs
@@ -6,6 +6,9 @@ using PoZiomkaDomain.StudentAnswers.Dtos;
 using PoZiomkaInfrastructure.Exceptions;
 using System.Data;
 using Dapper;
+using Microsoft.Extensions.Options;
+using PoZiomkaDomain.Form.Dtos;
+using System.Linq;
 
 namespace PoZiomkaInfrastructure.Repositories;
 
@@ -20,10 +23,82 @@ public class StudentAnswerRepository(IDbConnection connection) : IStudentAnswerR
     {
         throw new NotImplementedException();
     }
-
-    public Task<StudentAnswerDisplay> GetStudentAnswer(int formId, int studentId, CancellationToken? cancellationToken)
+    public record StudentAnswerObligatoryDto(int StudentAnswerObligatoryId, int ObligatoryPreferenceOptionId, bool IsHidden);
+    public record ObligatoryPreferenceDto(int ObligatoryPreferenceId, string ObligatoryPreferenceName);
+    public async Task<StudentAnswerDisplay> GetStudentAnswer(int formId, int studentId, CancellationToken? cancellationToken)
     {
-        throw new NotImplementedException();
+        var sql1 = @"SELECT Id From StudentAnswer WHERE StudentId = @studentId And FormId = @formId";
+        var sql2 = @"SELECT 
+                   s.Id as StudentAnswerObligatoryId, 
+                   s.ObligatoryPreferenceOptionId as ObligatoryPreferenceOptionId,
+	               s.IsHidden as IsHidden,
+       
+                   o.Id as ObligatoryPreferenceId,
+	               o.Name as ObligatoryPreferenceName,
+	   
+                   op.Id as Id,
+	               op.Name as Name
+	   
+	
+	                FROM StudentAnswerObligatory as s
+	                JOIN ObligatoryPreferences as o on s.ObligatoryPrefernceId = o.Id 
+	                JOIN ObligatoryPreferenceOptions as op on op.PreferenceId = o.Id 
+	                WHERE AnswerId = @answerId ";
+        int answerId;
+        try
+        {
+            answerId = await connection.QuerySingleOrDefaultAsync<int>(sql1, new { studentId, formId });
+        }
+        catch (SqlException exception)
+        {
+            throw new QueryExecutionException(exception.Message, exception.Number);
+        }
+
+        var lookup = new Dictionary<int, StudentAnswerObligatoryDisplay>();
+
+        try
+        {
+            var result = await connection.QueryAsync<
+                StudentAnswerObligatoryDto,
+                ObligatoryPreferenceDto,
+                ObligatoryPreferenceOptionDisplay,
+                StudentAnswerObligatoryDisplay
+            >(sql2,
+            (studentDto, preferenceDto, optionDto) =>
+            {
+                if (!lookup.TryGetValue(studentDto.StudentAnswerObligatoryId, out var answer))
+                {
+                    answer = new StudentAnswerObligatoryDisplay(
+                        Id: studentDto.StudentAnswerObligatoryId,
+                        ObligatoryPreference: new ObligatoryPreferenceDisplay(
+                            Id: preferenceDto.ObligatoryPreferenceId,
+                            Name: preferenceDto.ObligatoryPreferenceName,
+                            Options: new List<ObligatoryPreferenceOptionDisplay>()
+                        ),
+                        ObligatoryPreferenceOptionId: studentDto.ObligatoryPreferenceOptionId,
+                        IsHidden: studentDto.IsHidden
+                    );
+                    lookup[studentDto.StudentAnswerObligatoryId] = answer;
+                }
+
+                var options = (List<ObligatoryPreferenceOptionDisplay>)answer.ObligatoryPreference.Options;
+                if (!options.Any(o => o.Id == optionDto.Id))
+                {
+                    options.Add(optionDto);
+                }
+
+                return answer;
+            },
+            splitOn: "ObligatoryPreferenceId,Id",
+            param: new { answerId });
+        }
+        catch (SqlException exception)
+        {
+            throw new QueryExecutionException(exception.Message, exception.Number);
+        }
+
+        StudentAnswerDisplay display = new StudentAnswerDisplay(answerId, formId, studentId, new List<StudentAnswerChoosableDisplay>(), lookup.Values.ToList());
+        return display ?? throw new IdNotFoundException();
     }
 
     public async Task<IEnumerable<StudentAnswerStatus>> GetStudentFormAnswerStatus(int studentId, CancellationToken? cancellationToken)
