c49b05318a4082f477016d52cbf3fbfcf11e5d0a
pietraldo <pietrzakjakub4321@gmail.com>
2025-05-04 23:24:17 +0200

    auth update

--- PoZiomkaDomain/Match/Commands/UpdateMatchCommand/UpdateMatchCommandHandler.cs
+++ PoZiomkaDomain/Match/Commands/UpdateMatchCommand/UpdateMatchCommandHandler.cs
@@ -16,6 +16,11 @@ public class UpdateMatchCommandHandler(IMatchRepository matchRepository) : IRequ
     {
         int studentId = request.User.GetUserId() ?? throw new DomainException("UserId is null");
         MatchStatus status = request.isAccepted ? MatchStatus.Accepted : MatchStatus.Rejected;
+
+        var match = await matchRepository.GetStudentMatches(studentId);
+        if (match.All(x => x.Id != request.Id))
+            throw new DomainException("Match not found or it is not this studentId match");
+
         await matchRepository.UpdateMatch(request.Id, studentId, status);
     }
 }
