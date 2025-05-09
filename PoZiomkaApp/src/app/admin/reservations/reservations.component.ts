97c58d628fb1fed5a61e26220e127b15b1722c0e
NotAnCookie <160712740+NotAnCookie@users.noreply.github.com>
2025-05-09 12:51:46 +0200

    admin-reservations base

--- PoZiomkaApp/src/app/admin/reservations/reservations.component.ts
+++ PoZiomkaApp/src/app/admin/reservations/reservations.component.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, NgModule} from '@angular/core';
+import { ReservationModel, ReservationStudentModel } from '../../reservation/reservation.model';
+import { ReservationService } from '../../reservation/reservation.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-reservations',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './reservations.component.html',
   styleUrl: './reservations.component.css'
 })
-export class ReservationsComponent {
+export class ReservationsComponent implements OnInit {
+  reservations: ReservationStudentModel[] = [];
+  isLoading = true;
 
+  constructor(private reservationService: ReservationService) {}
+
+  ngOnInit(): void {
+    this.loadReservationsWithDetails();
+  }
+
+  loadReservationsWithDetails() {
+    this.isLoading = true;
+    this.reservationService.getReservations().subscribe(response => {
+      const reservationList = response.payload ?? [];
+
+      // pobieramy dane szczegółowe dla każdej rezerwacji
+      const detailRequests = reservationList.map(r =>
+        this.reservationService.getReservation(r.id)
+      );
+
+      Promise.all(detailRequests.map(obs => obs.toPromise()))
+        .then(responses => {
+          this.reservations = responses.map(res => res?.payload!).filter(Boolean);
+          this.isLoading = false;
+        })
+        .catch(() => this.isLoading = false);
+    });
+  }
+
+  toggleAccept(reservationId: number, currentStatus: boolean) {
+    const newStatus = !currentStatus;
+    this.reservationService.updateReservation(reservationId, newStatus).subscribe(() => {
+      const reservation = this.reservations.find(r => r.id === reservationId);
+      if (reservation) reservation.isAcceptedByAdmin = newStatus;
+    });
+  }
 }
